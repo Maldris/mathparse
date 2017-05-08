af4d40c0bb449e2f97a224e17abff6ce5f906bfe
Maldris <sdark.arrow@gmail.com>
2017-05-08 23:24:17 +1000

    moved output logic to main, isnt resolver based logic, belongs slightly better in main. added a function to get the tokens out, in the event thats useful to certain use cases.

--- main.go
+++ main.go
@@ -5,3 +5,42 @@ func NewParser(expression string) Parser {
 	parse.ReadExpression(expression)
 	return parse
 }
+
+func (p *Parser) FoundResult() bool {
+	return len(p.tokens) <= 1 && p.tokens[0].Type == literal
+}
+
+func (p *Parser) GetValueResult() float64 {
+	return p.tokens[0].ParseValue
+}
+
+func (p *Parser) GetExpressionResult() string {
+	return getStringExpression(p.tokens)
+}
+
+func getStringExpression(set []Token) string {
+	str := ""
+	for _, tok := range set {
+		switch tok.Type {
+		case space:
+			str += " "
+		case literal:
+			str += tok.Value
+		case variable:
+			str += tok.Value
+		case operation:
+			str += tok.Value
+		case function:
+			str += tok.Value + "(" + getStringExpression(tok.Children) + ")"
+		case lparen:
+			str += "(" + getStringExpression(tok.Children) + ")"
+		case funcDelim:
+			str += ","
+		}
+	}
+	return str
+}
+
+func (p *Parser) GetTokens() []Token {
+	return p.tokens
+}
--- resolve.go
+++ resolve.go
@@ -15,41 +15,6 @@ func (p *Parser) Resolve() {
 	p.tokens = parseExpression(p.tokens)
 }
 
-func (p *Parser) FoundResult() bool {
-	return len(p.tokens) <= 1 && p.tokens[0].Type == literal
-}
-
-func (p *Parser) GetValueResult() float64 {
-	return p.tokens[0].ParseValue
-}
-
-func (p *Parser) GetExpressionResult() string {
-	return getStringExpression(p.tokens)
-}
-
-func getStringExpression(set []Token) string {
-	str := ""
-	for _, tok := range set {
-		switch tok.Type {
-		case space:
-			str += " "
-		case literal:
-			str += tok.Value
-		case variable:
-			str += tok.Value
-		case operation:
-			str += tok.Value
-		case function:
-			str += tok.Value + "(" + getStringExpression(tok.Children) + ")"
-		case lparen:
-			str += "(" + getStringExpression(tok.Children) + ")"
-		case funcDelim:
-			str += ","
-		}
-	}
-	return str
-}
-
 func parseExpression(set []Token) []Token {
 	mod := false
 	if set[0].Type == function || set[0].Type == lparen {
